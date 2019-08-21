049b30e5ebbf478db7d4deec75b0eb88422bcd3d
allisonnnnnn <44420135+allisonnnnnn@users.noreply.github.com>
2019-08-21 15:11:17 -0400

    Update eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,33 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+
+// const eqObjects = function(obj1, obj2) {
+//   let obj1K = Object.keys(obj1);
+//   let obj2K = Object.keys(obj2);
+//   if (obj1K.length !== obj2K.length) {
+//     return false;
+//   }
+
+//   for (let key of obj1K) {
+//     if (typeof obj1[key] !== typeof obj2[key]) {
+//       return false;
+//     } else if (typeof obj1[key] === typeof obj2[key]) {
+//       if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+//         if (eqArrays(obj1[key], obj2[key]) === false) {
+//           return false;
+//         }
+//       } else {
+//         if (obj1[key] !== obj2[key]) {
+//           return false;
+//         }
+//       }
+//     }
+//   }
+
+//   return true;
+// };
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
