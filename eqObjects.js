471c3edd9d92581368e26c0338eb56483f41eeb0
allisonnnnnn <44420135+allisonnnnnn@users.noreply.github.com>
2019-08-25 22:01:33 -0400

    Update eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,10 +1,10 @@
-//const assertEqual = function(actual, expected) {
-  //if (actual === expected) {
-    //console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  //} else {
-    //console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
- // }
-//};
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+ }
+};
 
 
 // const eqObjects = function(obj1, obj2) {
