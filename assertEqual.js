f9648417bb93c3ec1c91ebc99372f01a7679efc0
Allison <allison@Allisons-MacBook-Air.local>
2021-06-09 13:39:38 -0400

    Re-implemented assertEqausl

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,20 @@
+// FUNCTION IMPLEMENTATION
+
+// ASSERT STRING
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
+
+  let message = "";
+
+  // TEMPLATE LITERALS  
+  actual === expected ? message = `✅Assertion Passed: ${actual} === ${expected}`: message = `🛑Assertion Failed: ${actual} !== ${expected}`
+
+  return console.log(message)
 };
 
 module.exports = assertEqual;
 
+
+// TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
